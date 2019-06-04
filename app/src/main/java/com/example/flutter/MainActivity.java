65512b61cc4f9dc2b5c8312ee93097285aa3f0f2
czh998 <362200981@qq.com>
2019-06-05 00:31:04 +0800

    配置flutter

--- app/src/main/java/com/example/flutter/MainActivity.java
+++ app/src/main/java/com/example/flutter/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.flutter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import io.flutter.facade.Flutter;
+import io.flutter.view.FlutterView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FlutterView flutterView = Flutter.createView(this, getLifecycle(), "");
+        setContentView(flutterView);
+        View parent = (View) flutterView.getParent();
+        parent.setVisibility(View.INVISIBLE);
+        flutterView.addFirstFrameListener(() -> {
+            System.out.println("============================");
+            parent.setVisibility(View.VISIBLE);
+        });
     }
 }
